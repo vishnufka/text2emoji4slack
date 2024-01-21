7063715c07192783747fa7192c90ca1826c93e6e
c0gnac <sab128@gmail.com>
2024-01-20 18:45:58 -0800

    initial commit, transferring in

---
+++ text2emoji.js
@@ -0,0 +1,442 @@
+var a = [
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ]
+];
+
+var b = [
+[ 1,1,0 ],
+[ 1,0,1 ],
+[ 1,1,0 ],
+[ 1,0,1 ],
+[ 1,1,0 ]
+];
+
+var c = [
+[ 1,1,1 ],
+[ 1,0,0 ],
+[ 1,0,0 ],
+[ 1,0,0 ],
+[ 1,1,1 ]
+];
+
+var d = [
+[ 1,1,0 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,1,0 ]
+];
+
+var e = [
+[ 1,1,1 ],
+[ 1,0,0 ],
+[ 1,1,1 ],
+[ 1,0,0 ],
+[ 1,1,1 ]
+];
+
+var f = [
+[ 1,1,1 ],
+[ 1,0,0 ],
+[ 1,1,0 ],
+[ 1,0,0 ],
+[ 1,0,0 ]
+];
+
+var g = [
+[ 1,1,1,1 ],
+[ 1,0,0,0 ],
+[ 1,0,1,1 ],
+[ 1,0,0,1 ],
+[ 1,1,1,1 ]
+];
+
+var h = [
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ]
+];
+
+var i = [
+[ 1,1,1 ],
+[ 0,1,0 ],
+[ 0,1,0 ],
+[ 0,1,0 ],
+[ 1,1,1 ]
+];
+
+var j = [
+[ 1,1,1 ],
+[ 0,1,0 ],
+[ 0,1,0 ],
+[ 0,1,0 ],
+[ 1,1,0 ]
+];
+
+var k = [
+[ 1,0,0,1 ],
+[ 1,0,1,0 ],
+[ 1,1,0,0 ],
+[ 1,0,1,0 ],
+[ 1,0,0,1 ]
+];
+
+var l = [
+[ 1,0,0 ],
+[ 1,0,0 ],
+[ 1,0,0 ],
+[ 1,0,0 ],
+[ 1,1,1 ]
+];
+
+var m = [
+[ 1,1,1,1,1 ],
+[ 1,0,1,0,1 ],
+[ 1,0,1,0,1 ],
+[ 1,0,1,0,1 ],
+[ 1,0,1,0,1 ]
+];
+
+var n = [
+[ 1,0,0,0,1 ],
+[ 1,1,0,0,1 ],
+[ 1,0,1,0,1 ],
+[ 1,0,0,1,1 ],
+[ 1,0,0,0,1 ]
+];
+
+var o = [
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ]
+];
+
+var p = [
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ],
+[ 1,0,0 ],
+[ 1,0,0 ]
+];
+
+var q = [
+[ 1,1,1,1,0 ],
+[ 1,0,0,1,0 ],
+[ 1,0,1,1,0 ],
+[ 1,0,0,1,0 ],
+[ 1,1,1,1,1 ]
+];
+
+var r = [
+[ 1,1,1,1 ],
+[ 1,0,0,1 ],
+[ 1,1,1,1 ],
+[ 1,0,1,0 ],
+[ 1,0,0,1 ]
+];
+
+var s = [
+[ 1,1,1 ],
+[ 1,0,0 ],
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 1,1,1 ]
+];
+
+var t = [
+[ 1,1,1 ],
+[ 0,1,0 ],
+[ 0,1,0 ],
+[ 0,1,0 ],
+[ 0,1,0 ]
+];
+
+var u = [
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ]
+];
+
+var v = [
+[ 1,0,0,0,1 ],
+[ 1,0,0,0,1 ],
+[ 0,1,0,1,0 ],
+[ 0,1,0,1,0 ],
+[ 0,0,1,0,0 ]
+];
+
+var w = [
+[ 1,0,1,0,1 ],
+[ 1,0,1,0,1 ],
+[ 1,0,1,0,1 ],
+[ 1,0,1,0,1 ],
+[ 1,1,1,1,1 ]
+];
+
+var x = [
+[ 1,0,0,0,1 ],
+[ 0,1,0,1,0 ],
+[ 0,0,1,0,0 ],
+[ 0,1,0,1,0 ],
+[ 1,0,0,0,1 ]
+];
+
+var y = [
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ],
+[ 0,1,0 ],
+[ 0,1,0 ]
+];
+
+var z = [
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 0,1,0 ],
+[ 1,0,0 ],
+[ 1,1,1 ]
+];
+
+var zero = [
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ]
+];
+
+var one = [
+[ 0,1,0 ],
+[ 1,1,0 ],
+[ 0,1,0 ],
+[ 0,1,0 ],
+[ 1,1,1 ]
+];
+
+var two = [
+[ 1,1,0 ],
+[ 0,0,1 ],
+[ 0,1,0 ],
+[ 1,0,0 ],
+[ 1,1,1 ]
+];
+
+var three = [
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 1,1,1 ]
+];
+
+var four = [
+[ 1,0,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 0,0,1 ]
+];
+
+var five = [
+[ 1,1,1 ],
+[ 1,0,0 ],
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 1,1,0 ]
+];
+
+var six = [
+[ 0,0,1 ],
+[ 0,1,0 ],
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ]
+];
+
+var seven = [
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 0,1,0 ],
+[ 1,0,0 ],
+[ 1,0,0 ]
+];
+
+var eight = [
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ]
+];
+
+var nine = [
+[ 1,1,1 ],
+[ 1,0,1 ],
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 0,0,1 ]
+];
+
+var em = [
+[ 1 ],
+[ 1 ],
+[ 1 ],
+[ 0 ],
+[ 1 ]
+];
+
+var qm = [
+[ 1,1,1 ],
+[ 0,0,1 ],
+[ 0,1,0 ],
+[ 0,0,0 ],
+[ 0,1,0 ]
+];
+
+var sp = [
+[ 0 ],
+[ 0 ],
+[ 0 ],
+[ 0 ],
+[ 0 ]
+];
+
+var fs = [
+[ 0 ],
+[ 0 ],
+[ 0 ],
+[ 0 ],
+[ 1 ]
+];
+
+//not using zeroeth
+//var chars = [[],a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+//var digits = [zero,one,two,three,four,five,six,seven,eight,nine];
+//var punctuation = [sp,fs,em,qm];
+
+var xx = [[]];
+			  //-32 from the charCode
+var asciiArr = 	 [sp,em,xx,xx,xx,
+				  xx,xx,xx,xx,xx,
+				  xx,xx,xx,xx,fs,
+				  xx,zero,one,two,three,
+				  four,five,six,seven,eight,
+				  nine,xx,xx,xx,xx,
+				  xx,qm,xx,a,b,
+				  c,d,e,f,g,
+				  h,i,j,k,l,
+				  m,n,o,p,q,
+				  r,s,t,u,v,
+				  w,x,y,z,xx,
+				  xx,xx,xx,xx,xx,
+				  a,b,c,d,e,
+				  f,g,h,i,j,
+				  k,l,m,n,o,
+				  p,q,r,s,t,
+				  u,v,w,x,y,
+				  z,xx,xx,xx,xx,
+				  xx];
+
+$('#textToConvert').keyup(function(e) {
+	onLineInput();
+});
+
+$('#emoji').keyup(function(e) {
+	onLineInput();
+});
+
+$('#whitespace').keyup(function(e) {
+	onLineInput();
+});
+
+function onLineInput(){
+	//$("#current-characters").text(convertToEmoji(convertToAscii($("#input").val())).length);
+};
+
+// main code entry point
+function onSubmitButtonClick(){
+	updateTextAreaWithEmoji(convertToEmoji(convertToAscii($("#textToConvert").val())));
+};
+
+// listens for enter key
+document.onkeydown=function(evt) {
+	var keyCode = evt.which || evt.keyCode;
+	if (keyCode == 13) {
+		onSubmitButtonClick();
+	}
+};
+
+function convertToEmoji(input){
+	var finalStr = '';
+	var inputArr = [];
+	inputArr.push(input);
+
+	for (var ii = 0; ii < inputArr.length; ii++){
+		strRawArr = makeRawStringArr(inputArr[ii]);
+		finalStr += makeRow(strRawArr);
+		if (ii+1 !== inputArr.length){
+			finalStr += ':'+$("#whitespace").val()+':'+'\n';
+		}
+	}
+	return finalStr;
+};
+
+function updateTextAreaWithEmoji(str){
+	$("#output").text(function(){
+		if (str.length > 4000){
+			$(this).text("Too long, slack limit is 4000 chars, yours is " + str.length);
+		}
+		else{
+			$(this).text(str);
+		}
+	});
+};
+
+// converts text to ascii codes
+function convertToAscii(input){
+	var inputArr = [];
+	for (var ii = 0; ii < input.length; ii++){
+		var charCode = input[ii].charCodeAt(0);
+		inputArr.push(asciiArr[charCode-32]);
+	}
+	return inputArr;
+};
+
+// turns the input into a raw string
+function makeRawStringArr(strArr){
+	var strRawArr = [];
+	for (var ii = 0; ii < strArr.length; ii++){
+		strRawArr.push(strArr[ii]);
+		strRawArr.push(sp);
+	};
+	return strRawArr;
+};
+
+// makes a single row - there are five rows in each line
+function makeRow(strRaw){
+	var str = '';
+	for (var ii = 0; ii < 5; ii++){ // rows top to bottom, always 5
+		for (var jj = 0; jj < strRaw.length; jj++){ // characters
+			for (var kk = 0; kk < strRaw[jj][ii].length; kk++){
+				var bool = strRaw[jj][ii][kk]; // note the swap of ii and jj
+				if (bool){
+					str += ':'+$("#emoji").val()+':';
+				}
+				else {
+					str += ':'+$("#whitespace").val()+':';
+				}
+			}
+		}
+		str += '\n';
+	}
+	return str;
+};
